596bbc26e7280dfa2c854e9390e7a2db1c7d7ad7
Ying Qi Yeoh <yqyeoh@Ying-Qi-Yeohs-MacBook-Pro.local>
2014-12-01 16:33:36 +0800

    first rev

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Ying Qi Yeoh on 12/1/14.
+//  Copyright (c) 2014 Ying Qi Yeoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
